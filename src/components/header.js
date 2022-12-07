cabbca7f495ebfb6ce2adf270b8e3616c46bada0
Jeff McCracken <jjmccrac@gmail.com>
2022-12-07 12:37:46 -0800

    Added pics and text to home

--- src/components/header.js
+++ src/components/header.js
@@ -10,29 +10,29 @@ const Header = () => {
         <Navbar dark color='primary' sticky='top' expand='md'>
             <NavbarBrand lastName='ms-5' href='/' >
                 <img src='' alt='RR logo' className='float-start'></img>
-                <h1 className='mt-1'>Route Runners Moving Services</h1>
+                <h1 className='mt-1'>Route Runners</h1>
             </NavbarBrand>
 
             <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
             <Collapse isOpen={menuOpen} navbar>
                 <Nav className='ms-auto' navbar>
                     <NavItem>
-                        <NavLink className='nav-link' to='/'>
+                        <NavLink className='nav-link float-end' to='/'>
                             <i className='fa fa-home fa-lg' /> Home
                         </NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink className='nav-link' to='/directory'>
-                            <i className='fa fa-list fa-lg' /> Services
+                        <NavLink className='nav-link float-end' to='/services'>
+                            <i className='fa fa-truck fa-lg' /> Services
                         </NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink className='nav-link' to='/about'>
-                            <i className='fa fa-info fa-lg' /> About
+                        <NavLink className='nav-link float-end' to='/about'>
+                            <i className='fa fa-list fa-lg' /> About
                         </NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink className='nav-link' to='/contact'>
+                        <NavLink className='nav-link float-end' to='/contact'>
                             <i className='fa fa-address-card fa-lg' /> Contact
                         </NavLink>
                     </NavItem>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import openRoad from '../images/open-road.png';
+import { Row, Col } from 'reactstrap';
+import countyMap from '../images/County Map.png';
+import insertWide from '../images/Insert_wide-1-768x366.png';
+import reclaimSpace from '../images/ReclaimSpace.png';
 
 const Home = () => {
   return (
     <>
-    <div className='home-page'>Home</div>
+    <div className='home-page'>
+      <Row>
+        <img src={insertWide} alt="Protecting floors, carpets, and items in truck" className="img-fluid" id="insertwide" />
+      </Row>
+      <Row>
+        <Col className='col-12 col-sm-6'>
+          <img src={countyMap} alt="Map of Clark County, NV" id="countymap" className='img-fluid' />
+        </Col>
+        <Col className='col-12 col-sm-6'>
+          <p>Route Runners LLC is a family owned business that serves Las Vegas, Henderson, North Las Vegas, and all of Clark County. We are expanding to handle out of state clients as well. We are avaiable for commercial and residential customers. If you are in need of Moving Services, Junk Removal, or Hauling, we can help. At Route Runners . . . </p>
+          <p>"All Routes Lead to You!" </p>
+          <div className='col-12'>
+          <img src={reclaimSpace} alt="Before and after images of junk removal service" className='img-fluid' id="claimspace" />
+          </div>
+        </Col>
+      </Row>
+    </div>
     </>
   )
 }
