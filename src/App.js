ff94d380cbd0e760cdf64f1599e8b0f2eeb2dbca
Jeff McCracken <jjmccrac@gmail.com>
2022-12-15 11:08:39 -0800

    Minor changes, added build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
